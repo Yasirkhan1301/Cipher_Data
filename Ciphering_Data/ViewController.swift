81e7e9d142bd66c37c3b95d5596c0da2bbfaaec5
yasirkhan1301 <mr_yasir_khan52@yahoo.com>
2020-10-10 17:52:11 +0500

    shift_cipher_complete

--- Ciphering_Data/ViewController.swift
+++ Ciphering_Data/ViewController.swift
@@ -8,6 +8,7 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    var shift = Bool()
 
     @IBOutlet weak var Plain_text: NSTextField!
     
@@ -27,10 +28,22 @@ class ViewController: NSViewController {
 
 
     @IBAction func Cipher(_ sender: Any) {
+        if (sender as AnyObject).tag == 1
+        {
+            shift = true
+        }
         
     }
     
     @IBAction func encryption(_ sender: Any) {
+        let plain = String(Plain_text.stringValue)
+        let key = Int(Key_text.stringValue)
+        
+        if shift == true {
+            let s = Shift_Cipher()
+            let t = s.shiftCipherEncrypt(value: plain, shift: key!)
+            Cipher_text.stringValue = t
+        }
     }
     
     @IBAction func decryption(_ sender: Any) {
