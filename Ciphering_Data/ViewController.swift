2f37eb03b28a996342d5a56e418ac4e789324ae7
yasirkhan1301 <mr_yasir_khan52@yahoo.com>
2020-10-10 18:01:39 +0500

    viginere complete

--- Ciphering_Data/ViewController.swift
+++ Ciphering_Data/ViewController.swift
@@ -9,6 +9,7 @@ import Cocoa
 
 class ViewController: NSViewController {
     var shift = Bool()
+    var viginere = Bool()
 
     @IBOutlet weak var Plain_text: NSTextField!
     
@@ -31,6 +32,12 @@ class ViewController: NSViewController {
         if (sender as AnyObject).tag == 1
         {
             shift = true
+            viginere = false
+        }
+        if (sender as AnyObject).tag == 2
+        {
+            viginere = true
+            shift = false
         }
         
     }
@@ -44,9 +51,29 @@ class ViewController: NSViewController {
             let t = s.shiftCipherEncrypt(value: plain, shift: key!)
             Cipher_text.stringValue = t
         }
+        else if viginere == true
+        {
+            let v = Vigenere(key:Key_text.stringValue)
+            let x = v.encrypt(plainText: plain)
+            Cipher_text.stringValue = x
+        }
     }
     
     @IBAction func decryption(_ sender: Any) {
+        let plain = String(Plain_text.stringValue)
+        let key = Int(Key_text.stringValue)
+        
+        if shift == true {
+            let s = Shift_Cipher()
+            let t = s.shiftCipherDecrypt(value: plain, shift: key!)
+            Cipher_text.stringValue = t
+        }
+        else if viginere == true{
+            let v = Vigenere(key:Key_text.stringValue)
+            let x = v.decrypt(encryptedText: plain)
+            Cipher_text.stringValue = x
+        }
+        
     }
     
     
